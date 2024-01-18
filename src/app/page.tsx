20740fcb7136836f639b44c2298bc3739259e89c
Anna Apkhanova <anna.karaseva.91@inbox.ru>
2024-01-18 18:10:48 +0100

    first add swr for cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,33 @@ import Form from '@/components/Form'
 import Card from '@/components/Card'
 import { IForm, IVacancy } from '@/interfaces/models'
 import { useEffect, useState } from 'react';
+import useSWR from 'swr';
 
 export default function Home () {
   const [vacancies, setVacancies] = useState<IVacancy[]>([])
+
+  const fetcher = (url: string) => fetch(url)
+  .then((res) => res.json())
+  .then(response => setVacancies(response.data));
+  
+  const { error, isLoading } = useSWR('/api/vacancies', fetcher)
   
   useEffect(() => {
     getVacancies()
   }, [])
 
+  // if (error) return "An error has occurred.";
+  // if (isLoading) return "Loading...";
   return (
 
     <main className={styles.main}>
 
-      {vacancies?.map((vacancy) => (<Card key={vacancy.id} vacancy={vacancy} />))}
+      { error 
+        ? <p>An error has occurred.</p>
+        : isLoading
+        ? <p>Loading...</p>
+        : vacancies?.map((vacancy) => (<Card key={vacancy.id} vacancy={vacancy} />))
+      }
 
       <Form  handleSubmit={handleSubmit} />
 
